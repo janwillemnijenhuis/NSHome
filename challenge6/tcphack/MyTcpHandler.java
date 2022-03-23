69fe44e373d30b8d4afd9532bb835ab68a5bfa57
Jan Willem Nijenhuis <janwillem.nijenhuis@nedap.com>
2022-03-23 14:33:34 +0100

    connection established

--- challenge6/tcphack/MyTcpHandler.java
+++ challenge6/tcphack/MyTcpHandler.java
@@ -14,7 +14,7 @@ class MyTcpHandler extends TcpHandler {
 		boolean done = false;
 
 		// array of bytes in which we're going to build our packet:
-		int[] txpkt = new int[40];		// 40 bytes long for now, may need to expand this later
+		int[] txpkt = new int[60];		// 40 bytes long for now, may need to expand this later
 
 		txpkt[0] = 0x60;	// first byte of the IPv6 header contains version number in upper nibble
 		// fill in the rest of the packet yourself...:
@@ -24,23 +24,12 @@ class MyTcpHandler extends TcpHandler {
 
 		// Payload length
 		txpkt[4] = 0;
-		txpkt[5] = 0;
+		txpkt[5] = 20;
 		// Next header = TCP
-		txpkt[6] = 6;
+		txpkt[6] = 253;
 		// Hop limit
 		txpkt[7] = 64;
-
-		// setup src and destination address
-//		byte[] src = new byte[16];
-//		byte[] dest = new byte[16];
-//		InetAddress a = null;
-//		InetAddress b = null;
-//		try {
-//			a = InetAddress.getByName("2001:67c:2564:a303:6457:306b:fe93:e221");
-//			b = InetAddress.getByName("2001:610:1908:ff02:f57a:c534:9c8c:fbd4");
-//		} catch (UnknownHostException e) {
-//			e.printStackTrace();
-//		}
+		// setup src
 		txpkt[8] = 0x20;
 		txpkt[9] = 0x01;
 		txpkt[10] = 0x6;
@@ -57,7 +46,7 @@ class MyTcpHandler extends TcpHandler {
 		txpkt[21] = 0x93;
 		txpkt[22] = 0xe2;
 		txpkt[23] = 0x21;
-//		b = InetAddress.getByName("2001:610:1908:ff02:f57a:c534:9c8c:fbd4");
+		// setup dest
 		txpkt[24] = 0x20;
 		txpkt[25] = 0x01;
 		txpkt[26] = 0x6;
@@ -75,17 +64,27 @@ class MyTcpHandler extends TcpHandler {
 		txpkt[38] = 0xfb;
 		txpkt[39] = 0xd4;
 
-//		assert a != null;
-//		src = a.getAddress();
-//		assert b != null;
-//		dest = b.getAddress();
-//		System.out.println(src);
-//		System.out.println(dest);
-//		// Source address
-//		for (int i = 8; i < 24; i++) {
-//			txpkt[i] = src[i - 8];
-//			txpkt[i + 16] = dest [i - 8];
-//		}
+		/// TCP header
+		// 0000010011010010 scr port
+		txpkt[40] = 0b00000100;
+		txpkt[41] = 0b11010010;
+		// 0001111000011110  dest port
+		txpkt[42] = 0b00011110;
+		txpkt[43] = 0b00011110;
+		// sequence number
+		txpkt[44] = txpkt[45] = txpkt[46] = 0;
+		txpkt[47] = 0;
+		// ack number
+		txpkt[48] = txpkt[49] = txpkt[50] = 0;
+		txpkt[51] = 0;
+		// header length only first 4 bits count
+		txpkt[52] = 0b01010000;
+		// reserved + flags
+		txpkt[53] = 0b00000010;
+		// window size 0111000010000000
+		txpkt[54] = 0b01110000;
+		txpkt[55] = 0b10000000;
+
 
 		this.sendData(txpkt);	// send the packet
 
@@ -104,8 +103,18 @@ class MyTcpHandler extends TcpHandler {
 			// print the received bytes:
 			int i;
 			System.out.print("Received "+len+" bytes: ");
-			for (i=0;i<len;i++) System.out.print(rxpkt[i]+" ");
-			System.out.println("");
+			System.out.println(Integer.toBinaryString(rxpkt[53]));
+			int s1 = rxpkt[44];
+			int s2 = rxpkt[45];
+			int s3 = rxpkt[46];
+			int s4 = rxpkt[47] + 1;
+			txpkt[47] += 1;
+			txpkt[53] = 0b00010000;
+			txpkt[48] = s1;
+			txpkt[49] = s2;
+			txpkt[50] = s3;
+			txpkt[51] = s4;
+			this.sendData(txpkt);
 		}   
 	}
 }
